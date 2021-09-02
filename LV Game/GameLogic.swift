784310bad9a4ac405fbfe68ce06729f9869d7cea
Steffen Hauth <shauth@agtinternational.com>
2021-09-02 11:03:00 +0200

    Initial commit

---
+++ LV Game/GameLogic.swift
@@ -0,0 +1,135 @@
+//
+//  GameLogic.swift
+//  LV Game
+//
+//  Created by Steffen Hauth on 01.09.21.
+//
+
+import Foundation
+
+protocol GameLogicDelegate {
+    func update(idx: Int, value: Player)
+    func diceResult(w1: Int, w2: Int, value: Player, currentTurn: Player)
+}
+
+enum Player
+{
+    case Prey
+    case Predator
+    case empty
+}
+
+class GameLogic
+{
+    var rows: Int
+    var colums: Int
+    var numStartingFields: Int
+    var delegate: GameLogicDelegate
+    var fieldCollection: [Player]
+    var currentTurn: Player
+    
+    init(rows: Int, columns: Int, numStartingFields: Int, startingPlayer: Player, delegate: GameLogicDelegate)
+    {
+        self.rows = rows
+        self.colums = columns
+        self.numStartingFields = numStartingFields
+        self.delegate = delegate
+        self.fieldCollection = [Player](repeating: Player.empty, count: self.rows*self.colums) // 0 -> w -- 1 -> g -- 2 -> r
+        self.currentTurn = startingPlayer
+    }
+    
+    func initialValues()
+    {
+        let numFields = self.rows*self.colums
+        for idx in randArr(self.numStartingFields, numFields-1)
+        {
+            if Bool.random()
+            {
+                self.fieldCollection[idx] = Player.Prey
+                self.delegate.update(idx: idx, value: Player.Prey)
+            }
+            else
+            {
+                self.fieldCollection[idx] = Player.Predator
+                self.delegate.update(idx: idx, value: Player.Predator)
+            }
+        }
+    }
+    
+    func doRollingDice()
+    {
+        let w1 = Int.random(in: 0..<6)
+        let w2 = Int.random(in: 0..<6)
+        
+        let idx = w1 * self.colums + w2
+        
+        assert(idx < self.fieldCollection.count)
+        let val = self.fieldCollection[idx]
+        
+        self.delegate.diceResult(w1: w1, w2: w2, value: val, currentTurn: self.currentTurn)
+        
+        switch self.currentTurn
+        {
+        case Player.Predator:
+            let currentTurnString = "Predator"
+            switch val
+            {
+            case Player.empty:
+                // do nothing
+                NSLog("w1: \(w1) - w2: \(w2) - val: \(val) - turn: \(currentTurnString) --> Field is empty. Nothing happens.")
+            case Player.Prey:
+                NSLog("w1: \(w1) - w2: \(w2) - val: \(val) - turn: \(currentTurnString) --> Prey is on the field. Replaced by predator.")
+                self.delegate.update(idx: idx, value: Player.Predator)
+            case Player.Predator:
+                // do nothing
+                NSLog("w1: \(w1) - w2: \(w2) - val: \(val) - turn: \(currentTurnString) --> Predator is on the field.Nothing happens.")
+            }
+
+            self.currentTurn = Player.Prey
+
+        case Player.Prey:
+            let currentTurnString = "Prey"
+            switch val
+            {
+            case Player.empty:
+                NSLog("w1: \(w1) - w2: \(w2) - val: \(val) - turn: \(currentTurnString) --> Field is empty. Prey is set on that field.")
+                self.delegate.update(idx: idx, value: Player.Prey)
+            case Player.Prey:
+                NSLog("w1: \(w1) - w2: \(w2) - val: \(val) - turn: \(currentTurnString) --> Field occupied by Prey. Checking neighoring fields.")
+                let start1 = max(0, w1-1)
+                let end1 = min(self.rows - 1, w1 + 1)
+                let start2 = max(0, w2-1)
+                let end2 = min(self.colums - 1, w2 + 1)
+                for r in start1...end1
+                {
+                    for c in start2...end2
+                    {
+                        let i = r * self.colums + c
+                        assert(i < self.fieldCollection.count)
+                        let v = self.fieldCollection[i]
+                        NSLog("checking r: \(r) - c: \(c) - v: \(v)")
+                        if v == Player.empty
+                        {
+                            NSLog("field is empty -> putting prey on it")
+                            self.fieldCollection[i] = Player.Prey
+                            self.delegate.update(idx: i, value: Player.Prey)
+                            break
+                        }
+                    }
+                }
+            case Player.Predator:
+                // do nothing
+                NSLog("w1: \(w1) - w2: \(w2) - val: \(val) - turn: \(currentTurnString) --> Field is occupied by predator. Nothing happens.")
+            }
+
+            self.currentTurn = Player.Predator
+        default:
+            assert(false, "Default case should not be reached")
+        }
+    }
+    
+    private func randArr(_ elements: Int, _ max: Int) -> [Int]
+    {
+        return (0..<elements).map{ _ in Int.random(in: 0...max) }
+    }
+}
---
+++ LV Game/ViewController.swift
@@ -0,0 +1,137 @@
+//
+//  ViewController.swift
+//  LV Game
+//
+//  Created by Steffen Hauth on 17.08.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, GameLogicDelegate
+{
+    @IBOutlet weak var buttonNext: UIBarButtonItem!
+    var buttonCollection = [UIButton]()
+    var buttonStatus = UIButton()
+    
+    let rows = 6
+    let columns = 6
+    let numStartFields = 15
+    
+    var gameLogic: GameLogic?
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        debugPrint(self.view!.bounds.width)
+        debugPrint(self.view!.bounds.height)
+        
+        makeCheckboardButtons(rows: rows, columns: columns, totalWidth: Int(self.view!.bounds.width), totalHeight: Int(self.view!.bounds.height), showIndex: true)
+        
+        gameLogic = GameLogic(rows: self.rows, columns: self.columns, numStartingFields: self.numStartFields, startingPlayer: Player.Prey, delegate: self)
+        gameLogic?.initialValues()
+    }
+    
+    func makeCheckboardButtons(rows : Int, columns : Int, totalWidth : Int, totalHeight : Int, showIndex : Bool)
+    {
+        let buttonWidth = totalWidth / (columns + 1)
+        let offsetTop = 100
+        
+        buttonStatus = UIButton(frame: CGRect(x: 0, y: 50, width: totalWidth, height: buttonWidth))
+        buttonStatus.setAttributedTitle(NSMutableAttributedString(string: String("W1: W2:")), for: .normal)
+        buttonStatus.setTitleColor(UIColor.black, for: .normal)
+        buttonStatus.backgroundColor = UIColor.white
+        buttonStatus.layer.cornerRadius = 4
+        view.addSubview(buttonStatus)
+
+        var _rows = rows
+        var _columns = columns
+        if showIndex
+        {
+            _rows += 1
+            _columns += 1
+        }
+        for row in 0..<_rows
+        {
+            for column in 0..<_columns
+            {
+                if showIndex && row == 0
+                {
+                    let button = UIButton(frame: CGRect(x: buttonWidth*column, y: buttonWidth*row + offsetTop, width: buttonWidth, height: buttonWidth))
+                    button.backgroundColor = UIColor.white
+                    button.setTitleColor(UIColor.black, for: .normal)
+                    button.layer.cornerRadius = 4
+                    button.restorationIdentifier = "index_\(column)_\(row)"
+                    if column > 0
+                    {
+                        button.setAttributedTitle(NSMutableAttributedString(string: String(column)), for: .normal)
+                    }
+                    view.addSubview(button)
+                }
+                else if showIndex && column == 0
+                {
+                    let button = UIButton(frame: CGRect(x: buttonWidth*column, y: buttonWidth*row + offsetTop, width: buttonWidth, height: buttonWidth))
+                    button.backgroundColor = UIColor.white
+                    button.setTitleColor(UIColor.black, for: .normal)
+                    button.layer.cornerRadius = 4
+                    button.restorationIdentifier = "index_\(column)_\(row)"
+                    if row > 0
+                    {
+                        button.setAttributedTitle(NSMutableAttributedString(string: String(row)), for: .normal)
+                    }
+                    view.addSubview(button)
+                }
+                else
+                {
+                    let button = UIButton(frame: CGRect(x: buttonWidth*column, y: buttonWidth*row + offsetTop, width: buttonWidth, height: buttonWidth))
+                    button.backgroundColor = UIColor.white
+                    button.setTitleColor(UIColor.black, for: .normal)
+                    button.layer.cornerRadius = 4
+                    if showIndex
+                    {
+                        button.restorationIdentifier = "field_\((row-1)*columns + column-1)"
+                        button.setAttributedTitle(NSMutableAttributedString(string: String((row-1)*columns + column-1)), for: .normal)
+                    }
+                    else
+                    {
+                        button.restorationIdentifier = "field_\(row*columns+column)"
+                        button.setAttributedTitle(NSMutableAttributedString(string: String(row*columns+column)), for: .normal)
+                        
+                    }
+                    view.addSubview(button)
+                    buttonCollection.append(button)
+                }
+            }
+        }
+    }
+
+    func update(idx: Int, value: Player)
+    {
+        debugPrint("idx: \(idx) - value: \(value)")
+        switch value
+        {
+        case Player.empty:
+            buttonCollection[idx].backgroundColor = UIColor.white
+            buttonCollection[idx].setAttributedTitle(NSMutableAttributedString(string: String(idx)), for: .normal)
+        case Player.Prey:
+            buttonCollection[idx].backgroundColor = UIColor.green
+            buttonCollection[idx].setAttributedTitle(NSMutableAttributedString(string: String(idx)), for: .normal)
+        case Player.Predator:
+            buttonCollection[idx].backgroundColor = UIColor.red
+            buttonCollection[idx].setAttributedTitle(NSMutableAttributedString(string: String(idx)), for: .normal)
+        }
+    }
+    
+    func diceResult(w1: Int, w2: Int, value: Player, currentTurn: Player)
+    {
+        let valString = (value == Player.Predator) ? "Predator" : "Prey"
+        let turnString = (currentTurn == Player.Predator) ? "Predator" : "Prey"
+        buttonStatus.setAttributedTitle(NSMutableAttributedString(string: String("D1: \(w1) - D2: \(w2) - val: \(valString) - turn: \(turnString)")), for: .normal)
+    }
+    
+    @IBAction func onButtonNext(sender: UIBarButtonItem, forEvent event: UIEvent)
+    {
+        gameLogic?.doRollingDice()
+    }
+}
